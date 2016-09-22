2f65934714ef5c67ac9e31d912d034f0b1890d1e
Aleph Retamal <retamal.ph@gmail.com>
2016-09-23 09:44:03 +1000

    Release 0.0.1

---
+++ Example/Pods/Target Support Files/LLSpinner/LLSpinner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LLSpinner : NSObject
+@end
+@implementation PodsDummy_LLSpinner
+@end
---
+++ Example/Pods/Target Support Files/LLSpinner/LLSpinner-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "LLSpinner.h"
+
+FOUNDATION_EXPORT double LLSpinnerVersionNumber;
+FOUNDATION_EXPORT const unsigned char LLSpinnerVersionString[];
+
